fe12613f3c60cc15858c9a2b2bcf04e5866e3f6d
SilvioNardo <126958735+SilvioNardo@users.noreply.github.com>
2023-12-18 20:39:52 +0100

    diviso php da html

--- index.php
+++ index.php
@@ -1,43 +1,21 @@
 <?php 
 include("functions.php") ;
-$html = file_get_contents("header.html"); // Leggi il contenuto dell'HTML
+$header = file_get_contents("header.html"); // Leggi il contenuto dell'HTML
 
 $head_keywords = "Demeter, Raccolta, Rifiuti, Servizi"; // Valore da assegnare a {head_keywords}
-$html = str_replace("{head_keywords}", $head_keywords, $html);
+$header = str_replace("{head_keywords}", $head_keywords, $header);
 
 $head_description = "Servizio completo di trattamento dei rifiuti per ogni tipo di intervento. Garantiamo lavori rapidi, convenienti e a norma di legge"; // Valore da assegnare a {head_description}
-$html = str_replace("{head_description}", $head_description, $html);
+$header = str_replace("{head_description}", $head_description, $header);
 
-removeLinkFromNavItem($html, "home"); //  funzione per rimuovere link circolari
+removeLinkFromNavItem($header, "home"); //  funzione per rimuovere link circolari
 
 $breadcrumb_path = ""; // Valore da assegnare a {breadcrumb_path}
-$html = str_replace("{breadcrumb_path}", $breadcrumb_path, $html);
+$header = str_replace("{breadcrumb_path}", $breadcrumb_path, $header);
 
-echo $html; 
-?>
+echo $header; 
 
+$content = file_get_contents("contents/index.html");
+echo $content; //Leggi il contenuto del main nell'HTML
 
-<main id="content">
-
-            <h2>Gestisci i tuoi rifiuti nel migliore dei modi</h2>
-            <div id="wrapper">
-            <div class="left">
-            <p>
-                <strong>Demeter</strong> è l'azienda di riferimento per la <strong>raccolta</strong> e lo smaltimento dei <strong>rifiuti</strong> in tutta Italia.
-            </p>
-            <p>
-                Con i suoi <strong>servizi</strong>, Demeter riesce a garantire una completa assistenza e disponibilità nei confronti dei suoi clienti. 
-                Unisciti a noi accendendo al nostro portale e richiedendo subito il servizio che più ti interessa. Oppure contattaci direttamente 
-                all'indirizzo demeter@gmail.com per ottenere informazioni e chiarire i dubbi in merito al nostro servizio.
-            </p>
-            <p>
-                Consulta il <a href="calendar.html">calendario</a> e rimani aggiornato su tutto ciò che accade nel mondo, nel nostro mondo. Aiutaci a migliorare la nostra casa, 
-                in modo tale che sia vivibile ancora per migliaia di anni. Ognuno di noi, sebbene possa sembrare poco, è importante e insieme riusciremo 
-                a rendere questo mondo un posto migliore dove vivere per le prossime generazioni.
-            </p>
-        </div>
-        <div class="right"></div>
-     </div>
-        </main>
-
-<?php echo file_get_contents('footer.html'); ?>
+echo file_get_contents('footer.html'); ?>
--- news.php
+++ news.php
@@ -1,57 +1,21 @@
 <?php 
 include("functions.php") ;
-$html = file_get_contents("header.html"); // Leggi il contenuto dell'HTML
+$header = file_get_contents("header.html"); // Leggi il contenuto dell'HTML
 
 $head_keywords = "Demeter, Raccolta, Rifiuti"; // Valore da assegnare a {head_keywords}
-$html = str_replace("{head_keywords}", $head_keywords, $html);
+$header = str_replace("{head_keywords}", $head_keywords, $header);
 
 $head_description = "Servizio completo di trattamento dei rifiuti per ogni tipo di intervento. Garantiamo lavori rapidi, convenienti e a norma di legge"; // Valore da assegnare a {head_description}
-$html = str_replace("{head_description}", $head_description, $html);
+$header = str_replace("{head_description}", $head_description, $header);
 
-removeLinkFromNavItem($html, "news"); //  funzione per rimuovere link circolari
+removeLinkFromNavItem($header, "news"); //  funzione per rimuovere link circolari
 
 $breadcrumb_path = "&gt; &gt; <span lang=\"en\">News</span>"; // Valore da assegnare a {breadcrumb_path}
-$html = str_replace("{breadcrumb_path}", $breadcrumb_path, $html);
+$header = str_replace("{breadcrumb_path}", $breadcrumb_path, $header);
 
-echo $html; 
-?>
+echo $header; 
 
+$content = file_get_contents("contents/news.html");
+echo $content; //Leggi il contenuto del main nell'HTML
 
-<main id="content" class="news">
-        <h2>Ultime <span lang="en">news</span></h2>   
-        <article>
-            <h3> Emergenza alluvione - Demeter invia uomini e mezzi per lo sgombero e la pulizia nei Comuni alluvionati </h3>
-            <p>
-            Sono partiti lo scorso fine settimana gli operatori che aiuteranno nelle attività di rimozione di detriti e rifiuti che in questi giorni vengono accatastati lungo le strade. Verranno raccolti elettrodomestici, mobili e altri materiali che hanno subito danno a causa dell'acqua e di cui cittadini e aziende hanno bisogno di disfarsi al fine di poter liberare le strade.
-            Per due settimane gli operatori si alterneranno alla guida di 2 mezzi con ragno allestiti con due cassoni scarrabili ciascuno. 
-            <a href="../detailedNews/detailedNews.html?">Scopri di più</a>
-            </p>
-        </article> 
-        <article>
-            <h3> Si dà inizio al nuovo progetto didattico di sensibilizzazione sui rifiuti "Riciclo In Pratica"</h3>
-            <p>
-                Demeter, pioniere nell'ambiente sostenibile, ha lanciato "Riciclo In Pratica", un innovativo progetto didattico per sensibilizzare gli studenti sulla gestione responsabile dei rifiuti. Attraverso workshop, seminari e attività coinvolgenti, il programma mira a promuovere pratiche sostenibili nel quotidiano, dimostrando l'effetto positivo di piccoli gesti. 
-                La campagna #RicicloInPratica ha già ricevuto una risposta entusiastica, evidenziando l'impegno di Demeter nel promuovere una cultura consapevole e sostenibile nel lungo termine.
-                <a href="../detailedNews/detailedNews.html?">Scopri di più</a>
-                </p>
-        </article> 
-        <article>
-            <h3> EcoCalendari 2024 </h3>
-            <p>
-                Sono in consegna i nuovi calendari per l'anno 2024!
-                Le raccolte del nuovo anno sono già disponibili sul nostro sito, inoltre si può consultare l'intero calendario del 2024 tramite la nostra App.
-                Si ricorda ai cittadini di controllare attentamente l'EcoCalendario, perché in occasione delle prossime festività, in alcuni Comuni le raccolte subiranno delle variazioni.
-                <a href="../detailedNews/detailedNews.html?">Scopri di più</a>
-            </p>
-        </article> 
-        <article>
-            <h3> Nuove normative per i bidoni della spazzatura - Conosciamole Insieme </h3>
-            <p>
-                Demeter informa i cittadini sulle nuove normative per i bidoni della spazzatura, mirate a semplificare la raccolta differenziata e promuovere lo smaltimento responsabile dei rifiuti. I colori standard dei bidoni indicano le categorie specifiche, come carta, plastica, vetro, organico e rifiuti indifferenziati. 
-                L'adesione a queste norme è fondamentale per una gestione sostenibile dei rifiuti. Demeter invita i cittadini a seguire scrupolosamente le nuove disposizioni per contribuire a un ambiente più pulito e sano.
-                <a href="../detailedNews/detailedNews.html?">Scopri di più</a>
-                </p>
-        </article> 
-    </main>
-
-<?php echo file_get_contents('footer.html'); ?>
+ echo file_get_contents('footer.html'); ?>
