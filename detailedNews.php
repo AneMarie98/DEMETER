12c854a29d2638aac615e40b74717494283da36e
SilvioNardo <126958735+SilvioNardo@users.noreply.github.com>
2023-12-23 18:59:40 +0100

    aggiunti file mancanti (incompleti)

---
+++ detailedNews.php
@@ -0,0 +1,23 @@
+<?php
+    ini_set('display_errors',1);
+    ini_set('display_startup_errors',1);
+    setlocale(LC_ALL,'it_IT');
+
+    require_once "functions/dbAccess.php";
+
+    session_start();
+
+    $paginaHTML=file_get_contents("templates/detailedNewsTemplate.html");
+
+    if(isset($_SESSION["email"])){
+        $profile=$_SESSION["firstname"];
+        $profilelink="functions/logout.php";
+    }
+    else{
+        $profile="Accedi";
+        $profilelink="login.php";
+    }
+
+    $paginaHTML=str_replace("{profile}",$profile,$paginaHTML);
+    $paginaHTML=str_replace("{profilelink}",$profilelink,$paginaHTML);
+    echo $paginaHTML;
