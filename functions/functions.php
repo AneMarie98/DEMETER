0b98722bf92b35f2b8f5db1e5f3be0a3ca4670cb
SilvioNardo <126958735+SilvioNardo@users.noreply.github.com>
2024-01-20 03:26:31 +0100

    la pagina news non ha più img ma div. il path delle immagini a tali div è settato dinamicamente nel css tramite php. il php evita di creare duplicazione di tale codice.

--- functions/functions.php
+++ functions/functions.php
@@ -19,3 +19,41 @@ function convertDateFormatString($date){
     return $date;
 }
 
+
+function dynamicAppendCSS($cssFilePath, $cssContent) {
+    echo '<script>'; //tmp
+    echo 'console.log(\'dynamicAppend run\')'; //tmp
+    echo '</script>'; //tmp
+    // Verifica se il file CSS esiste
+    if (file_exists($cssFilePath)) {
+        // Ottieni il contenuto attuale del file CSS
+        $existingContent = file_get_contents($cssFilePath);
+        // Normalizzo i testi
+        $normalizedExistingContent = str_replace(["\r\n", "\r", "\n", ' '], '', $existingContent);
+        $normalizedCssContent = str_replace(["\r\n", "\r", "\n", ' '], '', $cssContent);
+ 
+        if (strpos($normalizedExistingContent, $normalizedCssContent) === false) {
+            echo '<script>'; //tmp
+            echo 'console.log(\'contenuto non trovato\')'; //tmp
+            echo '</script>'; //tmp
+
+            $pos = strpos($existingContent, '/* News */');
+            if ($pos !== false) {
+                // Posiziona il cursore dopo il commento
+                $pos += strlen('/* News */');
+                 // Inserisci il nuovo contenuto nella riga successiva
+                $newContent = substr_replace($existingContent, "\n\n".$cssContent."", $pos, 0);
+                file_put_contents($cssFilePath, $newContent);
+            }
+            else{
+                // Aggiungi il nuovo contenuto al file
+                file_put_contents($cssFilePath, $cssContent, FILE_APPEND);
+            }
+        }
+        else{ //tmp
+            echo '<script>'; //tmp
+            echo 'console.log(\'contenuto trovato\')'; //tmp
+            echo '</script>'; //tmp
+        }
+    }
+}
--- news.php
+++ news.php
@@ -4,6 +4,7 @@
     setlocale(LC_ALL,'it_IT');
 
     require_once "functions/dbAccess.php";
+    require_once "functions/functions.php";
 
     $db=new DB\DBAccess;
     $connOk=$db->openDBConnection();
@@ -25,13 +26,14 @@
        $newsFromDB = $db->getNews();
        if(is_array($newsFromDB) && count($newsFromDB) > 0){
         foreach ($newsFromDB as $news) {
+            // <img src=\"./img/news/".$news["urlImg"]."\" alt=\"\" >
             $htmlToInsert .= 
             "<article class >
-            <img src=\"./img/news/".$news["urlImg"]."\" alt=\"\" >
+            <div id=\"news_".$news["idNotizia"]."\" class=\"newsImg\"></div>
             <h3><a href=\"./detailedNews.php?id=".$news["idNotizia"]."\">". $news["titolo"] ."</a></h3>
             <p> ". $news["descrizione"]."</p>
             </article> ";
-            }
+            } 
        }else{
         $htmlToInsert .= "<p>Al momento non ci sono novità!</p>"; 
        }
@@ -40,6 +42,22 @@
     else{
         $htmlToInsert .= "<p>I nostri sistemi sono momentaneamente fuori servizi, stiamo lavorando per risolvere il problema.</p>"; 
     }
+
+    //inizio modifica al css
+    $cssStyles = "";
+    foreach ($newsFromDB as $news) {
+        echo '<script>'; //tmp
+        echo 'console.log(\'news\')'; //tmp
+        echo '</script>'; //tmp
+        $cssStyles = "#news_".$news["idNotizia"]."{\n\tbackground-image: url('../img/news/".$news["urlImg"]."');\n}";
+        
+        //desktop style
+        dynamicAppendCSS('css/style.css',$cssStyles);
+        //mini
+        dynamicAppendCSS('css/mini.css',$cssStyles);
+    }
+    
+    //fine modifica al css
 
     $paginaHTML=str_replace("{profile}",$profile,$paginaHTML);
     $paginaHTML=str_replace("{profilelink}",$profilelink,$paginaHTML);
