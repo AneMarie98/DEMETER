cf8f53c99ff88d35349c66ad60168371cf20d17c
EdoGallo02 <edogallo2002@gmail.com>
2024-01-27 14:42:46 +0100

    Ultimato index.php per mobile e desktop

--- js/togglemenu.js
+++ js/togglemenu.js
@@ -1,8 +1,10 @@
 function toggleMenu(){
     if(document.getElementById("menu").style.display=="block"){
         document.getElementById("menu").style.display="none";
+        document.getElementById("togglemenu").innerHTML="Apri menù";
     }
     else{
         document.getElementById("menu").style.display="block";
+        document.getElementById("togglemenu").innerHTML="Chiudi menù";
     }
 }
